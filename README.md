38e223e845ff2f9c0135c7751f044b218e498c65
Edward Roh <edroh90@gmail.com>
2018-08-07 13:59:39 -0700

    create-react-app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # TwitchTube
+
+yarn start
+Starts the development server.
+
+yarn build
+Bundles the app into static files for production.
+
+yarn test
+Starts the test runner.
--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('short'));
 
-app.use('/', express.static(path.join(__dirname, '../Client/dist')));
+app.use('/', express.static(path.join(__dirname, '../Client/public')));
 // app.use(router);
 
 var port = process.env.port || 1337;
